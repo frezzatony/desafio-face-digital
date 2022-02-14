4626aee452f1f824ceadafad874af8171d8d1128
frezzatony <frezzatony@gmail.com>
2022-02-14 20:32:17 -0300

    configuracoes iniciais do framework e layout

---
+++ application/views/templates/default/template.php
@@ -0,0 +1,7 @@
+<?php
+
+$this->load->view('templates/default/header');
+$this->load->view('templates/default/navbar');
+$this->load->view('templates/default/sidebar');
+echo $contents;
+$this->load->view('templates/default/footer');
